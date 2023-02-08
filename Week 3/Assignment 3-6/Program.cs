ef65e4455e4894e602aa3a6632382a1ac5ca076c
MagnusVV <svartkub@msn.com>
2023-02-08 14:21:09 +0100

    Finished tasks

---
+++ Week 3/Assignment 3-6/Program.cs
@@ -0,0 +1,33 @@
+﻿bool IsValidPassword(string passwordSuggestion)
+{
+    int digits = 0;
+
+    for (int i = 0; i < passwordSuggestion.Length; i++)
+    {
+        if (char.IsDigit(passwordSuggestion, i))
+        {
+            digits++;
+        }
+    }
+
+    if (!((passwordSuggestion.Length == digits) || digits < 2 || passwordSuggestion.Length < 8))
+    {
+        return true;
+    }
+
+    else
+    {
+        return false;
+    }
+}
+
+//Console.Write("Input password suggestion: ");
+//var passwordInput = Console.ReadLine();
+
+//Console.WriteLine(IsValidPassword(passwordInput));
+
+Console.WriteLine(IsValidPassword("short"));
+Console.WriteLine(IsValidPassword("password"));
+Console.WriteLine(IsValidPassword("12345678"));
+Console.WriteLine(IsValidPassword("xxxxxxx0"));
+Console.WriteLine(IsValidPassword("p4ssw0rd"));
