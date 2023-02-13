2705919d458814185f62d031ea3042a2c7f19938
MagnusVV <svartkub@msn.com>
2023-02-13 09:01:02 +0100

    Finished tasks

---
+++ Week 4/Assignment 1/Program.cs
@@ -0,0 +1,70 @@
+﻿var validator = new PasswordValidator();
+
+Console.Write("Input password suggestion: ");
+var passwordSuggestion = Console.ReadLine();
+
+if (validator.IsValidPassword(passwordSuggestion))
+{
+    Console.WriteLine($"the password {passwordSuggestion} is a VALID password");
+}
+else
+{
+    Console.WriteLine($"the password {passwordSuggestion} is an INVALID password");
+}
+
+
+class PasswordValidator {
+
+    public bool IsValidPassword(string input)
+    {
+        if (!HasMinimumLength(input, 8))
+        {
+            return false;
+        }
+
+        if (!HasNonDigits(input))
+        {
+            return false;
+        }
+
+        if (!HasAtLeastTwoDigits(input))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    bool HasMinimumLength(string input, int length)
+    {
+        return input.Length >= length;
+    }
+
+    bool HasNonDigits(string input)
+    {
+        for (var i = 0; i < input.Length; i++)
+        {
+            if (!char.IsDigit(input, i))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool HasAtLeastTwoDigits(string input)
+    {
+        var digits = 0;
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            if (char.IsDigit(input, i))
+            {
+                digits++;
+            }
+        }
+
+        return digits >= 2;
+    }
+};
---
+++ Week 4/Assignment 2/Program.cs
@@ -0,0 +1,30 @@
+﻿var movie = new Movie()
+{
+    Name = "Ghostbusters",
+    AgeRating = 11,
+    Price = 60
+};
+
+var cinema = new Cinema()
+{
+};
+
+cinema.MovieInformation(movie);
+
+
+
+class Movie
+{
+    public string Name { get; set; }
+    public int AgeRating = 0;
+    public int Price = 0;
+}
+
+class Cinema
+{
+
+    public void MovieInformation(Movie movie)
+    {
+        Console.WriteLine($"We are currently showing: {movie.Name} ({movie.AgeRating} years or older, {movie.Price}:-)");
+    }
+}
